d077c6aba51a0abc10f9002af7ca04806fa07291
Jason Sears <jsears4585@gmail.com>
2017-05-30 16:54:46 -0400

    added find_restaurant_by_name and find_unique restaurants, tested

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,19 +4,20 @@ require 'pry'
 
 # KFC single restaurant example
 class API_Comm
-  def find_by_restaurant_name name
-    rest = RestClient.get('https://data.cityofnewyork.us/resource/9w7m-hzhe.json?dba=' + name + ')'
+  def self.find_restaurant_by_name name
+    rest = RestClient.get('https://data.cityofnewyork.us/resource/9w7m-hzhe.json?dba=' + name)
     rest_json = JSON.parse(rest)
   end
 
   # need to add error handling for these methods
 
-  def find_unique_restaurants camis-num, json_results
+  def self.find_unique_restaurants json_results
     # check data type of camis-num, does this need to be string or num?
-    json_results.uniq {|inspection| inspection[camis-num.to_s]}
+    json_results.uniq {|inspection| inspection["camis"]}
   end
 
-  unique_kfc = all_kfc.uniq {|inspection| inspection["camis"]}
+  binding.pry
+
   brooklyn_kfcs = unique_kfc.select {|inspection| inspection["boro"] == "BROOKLYN"}
   kfcs_by_zip = unique_kfc.select {|inspection| inspection["zipcode"] == "10003"}
 end
