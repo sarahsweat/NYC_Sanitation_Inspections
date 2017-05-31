9a0a3f43446428c2110b80d095dedf5d19c5867b
Jason Sears <jsears4585@gmail.com>
2017-05-31 08:56:03 -0400

    Added parser class and search_restaurant_violations_by_id method

---
+++ lib/parser.rb
@@ -0,0 +1,23 @@
+require 'json'
+require 'pry'
+
+require_relative 'api_communicator.rb'
+
+class Parser
+
+  def self.search_restaurant_violations_by_id id, search_term
+    result = nil
+    regex = Regexp.new search_term
+    data = API_Comm.find_restaurant_data id
+    violation_data = API_Comm.select_all_violations data
+    joined = violation_data.join(" ")
+    if regex.match(joined)
+      puts "Search for " + search_term + " returned " + true.to_s
+    else
+      puts "Search for " + search_term + " returned " + false.to_s
+    end
+  end
+
+  search_restaurant_violations_by_id 41011367, "roaches"
+  
+end
