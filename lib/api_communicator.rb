b69fb2cca53cd840a8ac6190954d0a0b737c3202
Jason Sears <jsears4585@gmail.com>
2017-05-30 21:58:33 -0400

    Added Restaurant Unique ID methods

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -5,24 +5,33 @@ require 'pry'
 # KFC single restaurant example
 class API_Comm
 
+  # All Restaurant Results methods
   def self.find_restaurant_by_name name
     rest = RestClient.get('https://data.cityofnewyork.us/resource/9w7m-hzhe.json?dba=' + name)
-    rest_json = JSON.parse(rest)
+    all_restaurant_data = JSON.parse(rest)
   end
 
-  # need to add error handling for these methods
-
-  def self.find_unique_restaurants json_results
-    # check data type of camis-num, does this need to be string or num?
-    json_results.uniq {|inspection| inspection["camis"]}
+  def self.find_unique_restaurants all_restaurant_data
+    all_restaurant_data.uniq {|inspection| inspection["camis"]}
   end
 
-  def self.find_by_boro json_results, boro
-    json_results.select {|inspection| inspection["boro"] == boro.upcase}
+  def self.find_by_boro all_restaurant_data, boro
+    all_restaurant_data.select {|inspection| inspection["boro"] == boro.upcase}
   end
 
-  def self.find_by_zip json_results, zip
-    json_results.select {|inspection| inspection["zipcode"] == zip.to_s}
+  def self.find_by_zip all_restaurant_data, zip
+    all_restaurant_data.select {|inspection| inspection["zipcode"] == zip.to_s}
+  end
+
+  # Single Restaurant Unique ID methods
+  def self.find_restaurant_data id
+    # test with 41011367
+    rest = RestClient.get('https://data.cityofnewyork.us/resource/9w7m-hzhe.json?camis=' + id.to_s)
+    restaurant_data = JSON.parse(rest)
+  end
+
+  def self.select_all_violations restaurant_data
+     restaurant_data.map! {|x| x["violation_description"]}
   end
 
 end
